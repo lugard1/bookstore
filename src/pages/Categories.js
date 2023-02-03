a3a0091bd3b332cd5939d01e2fa1425b1958040d
Lugard Agu <agulugard@gmail.com>
2023-02-03 23:04:42 +0100

    update and add reducers and actions

---
+++ src/pages/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Card from '../components/UI/Card';
+
+const Categories = () => (
+  <Card>
+    <h2>Hello Form CategoriesPage</h2>
+    <button type="button">Check status</button>
+  </Card>
+);
+
+export default Categories;
